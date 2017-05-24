854a61c98572cd26589300b22d4beb52beaa0eb9
Tyler Hevia <tylerhevia@gmail.com>
2017-05-24 15:50:36 -0600

    finished editable fields, organized code

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ $("#save-button").on("click", clickSaveButton)
                  .on("click", disableSaveButton);
 $("#title-input, #body-input").on("input", disableSaveButton);
 $("#search-input").on("input", filterText);
-$("#idea-card-boyd").on("input", )
+$('#idea-container').on('blur', '.idea-card-title', saveChangesTitle)
+                    .on('blur', '.idea-card-body', saveChangesBody);
+$('#idea-container').on('keypress', '.idea-card-title', saveTitleOnClick);
+$('#idea-container').on('keypress', '.idea-card-body', saveBodyOnClick);
 
 function IdeaCard(title, body) {
   this.title = title;
@@ -20,7 +23,6 @@ function IdeaCard(title, body) {
 
 function clickSaveButton() {
   event.preventDefault();
-  console.log('yo');
   var titleInput = $("#title-input").val();
   var bodyInput = $("#body-input").val();
   var newCard = new IdeaCard(titleInput, bodyInput);
@@ -41,17 +43,27 @@ function createCard(ideaCard) {
       <div class="button-container">
         <button class="upvote" type="button" name="button"></button>
         <button class="downvote" type="button" name="button"></button>
-        <p class="quality-text">quality:${ideaCard.quality}</p>
+        <p class="quality-text">quality: ${ideaCard.quality}</p>
       </div>
     </article>`);
 }
-
-
 
 function reloadCards() {
   for (var i = 0; i < localStorage.length; i++) {
     createCard(JSON.parse(localStorage.getItem(localStorage.key(i))));
   }
+}
+
+function storeCard(idea) {
+  localStorage.setItem(idea.id, JSON.stringify(idea));
+}
+
+function setItemLocal(id, idea) {
+  localStorage.setItem(id, JSON.stringify(idea));
+}
+
+function removeCardFromStorage(ideaID) {
+  localStorage.removeItem(ideaID);
 }
 
 function removeCard() {
@@ -60,41 +72,27 @@ function removeCard() {
   removeCardFromStorage(ideaID);
 }
 
-function removeCardFromStorage(ideaID) {
-  localStorage.removeItem(ideaID);
-}
-
-function setItemLocal(id, idea) {
-  localStorage.setItem(id, JSON.stringify(idea));
-}
-
 function retrieveCard(ideaID) {
   var parsedObject = JSON.parse(localStorage.getItem(ideaID));
   return [parsedObject, parsedObject.qualities, parsedObject.index, parsedObject.quality];
 }
 
-function storeCard(idea) {
-  localStorage.setItem(idea.id, JSON.stringify(idea));
-}
-
 function upvote() {
   var ideaId = ($(this).closest(".idea-card").attr("id"));
   var idea = retrieveCard(ideaId);
-  console.log(idea);
-  if (idea[2] == idea[1].length - 1) {
+  if (idea[2] === idea[1].length - 1) {
     return;
   }
   idea[2]++;
   idea[0].quality = idea[1][idea[2]];
   idea[0].index = idea[2];
   setItemLocal(ideaId, idea[0]);
-  $(this).siblings('.quality-text').text("quality: " + " " + idea[1][idea[2]]);
+  $(this).siblings('.quality-text').text("quality: " + idea[1][idea[2]]);
 }
 
 function downvote() {
   var ideaId = ($(this).closest(".idea-card").attr("id"));
   var idea = retrieveCard(ideaId);
-  console.log(idea);
   if (idea[2] === 0) {
     return;
   }
@@ -102,13 +100,13 @@ function downvote() {
   idea[0].quality = idea[1][idea[2]];
   idea[0].index = idea[2];
   setItemLocal(ideaId, idea[0]);
-  $(this).siblings('.quality-text').text("quality: " + " " + idea[1][idea[2]]);
+  $(this).siblings('.quality-text').text("quality: " + idea[1][idea[2]]);
 }
 
 function disableSaveButton() {
   var titleInput = $("#title-input").val();
   var bodyInput = $("#body-input").val();
-  if (titleInput != "" && bodyInput != "" ) {
+  if (titleInput !== "" && bodyInput !== "" ) {
     $("#save-button").prop("disabled", false);
   } else if (titleInput === "" && bodyInput === "") {
     $("#save-button").prop("disabled", true);
@@ -121,37 +119,49 @@ function filterText() {
     var ideaText = $(this).text().toLowerCase();
     if(ideaText.indexOf(searchInput) !== -1) {
       $(this).show();
-      console.log(this);
     }
     else {
       $(this).hide();
-      console.log(this);
     }
   })
-  }
-  // trying to call saveChanges function when enter button is pressed while editing idea-card-body
-// $('#idea-container').on('keypress', '.idea-card-body', function(event) {
-//      event.preventDefault();
-//      if(event.keyCode === 13) {
-//      console.log("hello");
-//      saveChanges();
-//    }
-//   })
+}
 
-$('#idea-container').on('blur', '.idea-card-title', saveChanges)
-                    .on('blur', '.idea-card-body', saveChanges);
-
-
-function saveChanges() {
-  // declare variable for idea card's id
+function saveTitleOnClick(event) {
   var ideaID = ($(this).closest(".idea-card").attr("id"));
-  // use retrieveCard function with idea card's id as argument to retrieve object and set that to a variable
   var ideaObject = retrieveCard(ideaID);
-  // set variable.title = new input in idea card body and idea card title
-  var newIdeaObjectBody = $(".idea-card-body").text();
-  var newIdeaObjectTitle = $(".idea-card-title").text();
-  ideaObject[0].body = newIdeaObjectBody;
+  if(event.keyCode === 13) {
+    event.preventDefault();
+    var newIdeaObjectTitle = $(this).closest(".idea-card-title").text();
+    ideaObject[0].title = newIdeaObjectTitle;
+    setItemLocal(ideaID, ideaObject[0]);
+    $('.idea-card-title').blur();
+  }
+}
+
+function saveBodyOnClick(event) {
+  var ideaID = ($(this).closest(".idea-card").attr("id"));
+  var ideaObject = retrieveCard(ideaID);
+  if(event.keyCode === 13) {
+    event.preventDefault();
+    var newIdeaObjectBody = $(this).closest(".idea-card-body").text();
+    ideaObject[0].body = newIdeaObjectBody;
+    setItemLocal(ideaID, ideaObject[0]);
+    $('.idea-card-body').blur();
+  }
+}
+
+function saveChangesTitle() {
+  var ideaID = ($(this).closest(".idea-card").attr("id"));
+  var ideaObject = retrieveCard(ideaID);
+  var newIdeaObjectTitle = $(this).closest(".idea-card-title").text();
   ideaObject[0].title = newIdeaObjectTitle;
   setItemLocal(ideaID, ideaObject[0]);
-  //
+}
+
+function saveChangesBody() {
+  var ideaID = ($(this).closest(".idea-card").attr("id"));
+  var ideaObject = retrieveCard(ideaID);
+  var newIdeaObjectBody = $(this).closest(".idea-card-body").text();
+  ideaObject[0].body = newIdeaObjectBody;
+  setItemLocal(ideaID, ideaObject[0]);
 }
